39aea8c216f4a929f4ea0104e627350c31d4f0d9
Dilena Irene Grijalva Tenas <dilena13grijalva@gmail.com>
2025-05-23 18:22:12 -0600

    Add files via upload

---
+++ Proyecto Final 1/Proyecto Final 1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Proyecto_Final_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTema = new System.Windows.Forms.Label();
+            this.txtTema = new System.Windows.Forms.TextBox();
+            this.btnInvestigar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnGenerarword = new System.Windows.Forms.Button();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.btnGenerarPPT = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTema
+            // 
+            this.lblTema.AutoSize = true;
+            this.lblTema.Font = new System.Drawing.Font("Nirmala Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTema.Location = new System.Drawing.Point(33, 42);
+            this.lblTema.Name = "lblTema";
+            this.lblTema.Size = new System.Drawing.Size(210, 23);
+            this.lblTema.TabIndex = 0;
+            this.lblTema.Text = "¿En que puedo ayudarte?";
+            // 
+            // txtTema
+            // 
+            this.txtTema.Location = new System.Drawing.Point(277, 33);
+            this.txtTema.Multiline = true;
+            this.txtTema.Name = "txtTema";
+            this.txtTema.Size = new System.Drawing.Size(284, 43);
+            this.txtTema.TabIndex = 1;
+            // 
+            // btnInvestigar
+            // 
+            this.btnInvestigar.Font = new System.Drawing.Font("Nirmala Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInvestigar.Location = new System.Drawing.Point(644, 33);
+            this.btnInvestigar.Name = "btnInvestigar";
+            this.btnInvestigar.Size = new System.Drawing.Size(103, 64);
+            this.btnInvestigar.TabIndex = 2;
+            this.btnInvestigar.Text = "Consultar IA";
+            this.btnInvestigar.UseVisualStyleBackColor = true;
+            this.btnInvestigar.Click += new System.EventHandler(this.btnInvestigar_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Este es el resultado de tu pregunta";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(45, 209);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(631, 269);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // btnGenerarword
+            // 
+            this.btnGenerarword.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnGenerarword.Font = new System.Drawing.Font("Nirmala Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarword.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGenerarword.Location = new System.Drawing.Point(793, 299);
+            this.btnGenerarword.Name = "btnGenerarword";
+            this.btnGenerarword.Size = new System.Drawing.Size(150, 62);
+            this.btnGenerarword.TabIndex = 5;
+            this.btnGenerarword.Text = "Generar Word ";
+            this.btnGenerarword.UseVisualStyleBackColor = false;
+            this.btnGenerarword.Click += new System.EventHandler(this.btnGenerarword_Click);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(956, 471);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 16);
+            this.lblEstado.TabIndex = 6;
+            // 
+            // btnGenerarPPT
+            // 
+            this.btnGenerarPPT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnGenerarPPT.Font = new System.Drawing.Font("Nirmala Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarPPT.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGenerarPPT.Location = new System.Drawing.Point(793, 367);
+            this.btnGenerarPPT.Name = "btnGenerarPPT";
+            this.btnGenerarPPT.Size = new System.Drawing.Size(150, 65);
+            this.btnGenerarPPT.TabIndex = 7;
+            this.btnGenerarPPT.Text = "Generar Power Point";
+            this.btnGenerarPPT.UseVisualStyleBackColor = false;
+            this.btnGenerarPPT.Click += new System.EventHandler(this.btnGenerarPPT_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Nirmala Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(771, 33);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(101, 64);
+            this.btnLimpiar.TabIndex = 8;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 508);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGenerarPPT);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.btnGenerarword);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInvestigar);
+            this.Controls.Add(this.txtTema);
+            this.Controls.Add(this.lblTema);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTema;
+        private System.Windows.Forms.TextBox txtTema;
+        private System.Windows.Forms.Button btnInvestigar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnGenerarword;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Button btnGenerarPPT;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Proyecto Final 1/Proyecto Final 1/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using Microsoft.Office.Interop.Word;
+
+namespace Proyecto_Final_1
+{
+    public partial class Form1 : Form 
+    {
+
+        string apiKey = "AIzaSyBfLn9NzCjXzkzlAPa_qoIdN45O5f9FypE  ";
+
+
+        //campos 
+        private int borderRadius = 30;
+        private int borderSize = 2;
+        private Color borderColor = Color.RoyalBlue;
+
+        // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle= FormBorderStyle.None;
+            this.Padding = new Padding(borderSize);
+        }
+
+        // Método para redondear los bordes del formulario
+        [DllImport("user32.DLL",EntryPoint ="ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL",EntryPoint ="SendMessage")]
+        private extern static void SendMenssage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000; //Minimizar sin bordes desde la barra de tareas 
+                return cp;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMenssage(this.Handle, 0x112, 0xF012, 0); //Mover el formulario
+        }
+
+        //Metodo para redondear los bordes del formulario
+        private GraphicsPath GetRoundedPath(System.Drawing.Rectangle rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        private void FormRegionAndBorder(Form form, float radius, Graphics graph, Color borderColor, float borderSize)
+        {
+            if (form.WindowState != FormWindowState.Minimized)
+            {
+                using (GraphicsPath roundPath = GetRoundedPath(form.ClientRectangle, radius))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                using (Matrix transform = new Matrix())
+                {
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    form.Region = new Region(roundPath);
+                    if (borderSize > 1)
+                    {
+                        System.Drawing.Rectangle rect = form.ClientRectangle;
+                        float scaleX = 1.0F - ((borderSize + 1) / rect.Width);
+                        float scaleY = 1.0F - ((borderSize + 1) / rect.Height);
+
+                        transform.Scale(scaleX, scaleY);
+                        transform.Translate(borderSize / 1.6F, borderSize / 1.6F);
+
+                        graph.Transform = transform;
+                        graph.DrawPath(penBorder, roundPath);
+                    }
+                }
+            }
+        }
+   
+        //Llamar los parametros del borde para el formulario
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            FormRegionAndBorder(this, borderRadius, e.Graphics, borderColor, borderSize);
+        }
+
+        private async Task<string> ConsultarGeminiFlash(string prompt)
+        {
+            string url = $"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={apiKey}";
+
+            var requestData = new
+            {
+                contents = new[]
+                {
+                    new {
+                        parts = new[] {
+                            new { text = prompt }
+                        }
+                    }
+                }
+            };
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);// para convertir el objeto a formato json
+
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(json, Encoding.UTF8, "application/json");//para enviar el contenido en formato json
+                var response = await client.PostAsync(url, content);// para enviar la peticion a la API
+                var result = await response.Content.ReadAsStringAsync();// para leer la respuesta de la API
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = JObject.Parse(result);
+                    return data["candidates"]?[0]?["content"]?["parts"]?[0]?["text"]?.ToString() ?? "Sin respuesta";// para obtener el texto de la respuesta
+                }
+                else
+                {
+                    throw new Exception($"Error en la API: {result}");
+                }
+            }
+        }
+
+        private async void btnInvestigar_Click_1(object sender, EventArgs e)
+        {
+            string tema = txtTema.Text;
+            if (string.IsNullOrWhiteSpace(tema))
+            {
+                MessageBox.Show("Ingrese un tema.");
+                return;
+            }
+
+            try
+            {
+                string respuesta = await ConsultarGeminiFlash(tema);
+                txtResultado.Text = respuesta;
+
+                GuardarInvestigacion(tema, tema, respuesta);// Guardar en la base de datos
+                MessageBox.Show("Investigación guardada en la base de datos.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void GuardarInvestigacion(string titulo, string prompt, string respuesta)
+        {
+            using (SqlConnection conn = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog=Investigaciones; Integrated Security=True"))
+           
+            {
+                conn.Open();// abrir la conexion a la base de datos
+                string query = "INSERT INTO TEMAS (Titulo, Prompt, Respuesta) VALUES (@titulo, @prompt, @respuesta)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@titulo", titulo);
+                    cmd.Parameters.AddWithValue("@prompt", prompt);
+                    cmd.Parameters.AddWithValue("@respuesta", respuesta);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void CrearWord(string titulo, string contenido, string pie)
+        {
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            Document doc = wordApp.Documents.Add();
+            wordApp.Visible = false;
+
+            Paragraph titleParagraph = doc.Content.Paragraphs.Add();
+            titleParagraph.Range.Text = titulo;
+            titleParagraph.Range.Font.Bold = 1;// para poner el texto en negrita
+            titleParagraph.Range.Font.Size = 15;
+            titleParagraph.Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;// para alinear el titulo en el centro
+            titleParagraph.Range.InsertParagraphAfter();
+
+            Paragraph contentParagraph = doc.Content.Paragraphs.Add();
+            contentParagraph.Range.Text = contenido;
+            contentParagraph.Range.Font.Size = 12;
+            contentParagraph.Format.Alignment = WdParagraphAlignment.wdAlignParagraphRight;// contenido se alinee a la derecha
+            contentParagraph.Range.InsertParagraphAfter();
+           
+            doc.Words.Last.InsertBreak(WdBreakType.wdPageBreak);
+            Paragraph footerParagraph = doc.Content.Paragraphs.Add();
+            footerParagraph.Range.Text = pie;
+            footerParagraph.Range.Font.Italic = 1;// para poner el texto en cursiva
+            footerParagraph.Range.Font.Size = 10;
+            footerParagraph.Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;// para alinear el pie de pagina en el centro         
+
+            string ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "InvestigacionesIA", "Informe.docx");// para guardar el documento en la carpeta de documentos del usuario
+            Directory.CreateDirectory(Path.GetDirectoryName(ruta));// para crear la carpeta si no existe
+            doc.SaveAs2(ruta);// para guardar el documento
+
+            doc.Close();// cerrar el documento
+            wordApp.Quit();// para cerrar la aplicacion de word
+            Marshal.ReleaseComObject(doc);// liberar el objeto de word
+            Marshal.ReleaseComObject(wordApp);
+        }
+
+        //Para crear la presentacion en power point con le contenido de la base de datos 
+        private void CrearPowerPoint(string titulo, string contenido)
+        {
+            var app = new Microsoft.Office.Interop.PowerPoint.Application();
+            app.Visible = Microsoft.Office.Core.MsoTriState.msoTrue; 
+
+            var pres = app.Presentations.Add(Microsoft.Office.Core.MsoTriState.msoTrue); 
+            var slide = pres.Slides.Add(1, Microsoft.Office.Interop.PowerPoint.PpSlideLayout.ppLayoutText);// para agregar una diapositiva con el diseño de texto
+
+
+            slide.Shapes[1].TextFrame.TextRange.Text = titulo;
+            slide.Shapes[2].TextFrame.TextRange.Text = contenido;
+
+            string ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "InvestigacionesIA", "Presentacion.pptx");
+            pres.SaveAs(ruta);
+            pres.Close();
+            app.Quit();// cerrar la aplicacion de power point
+            Marshal.ReleaseComObject(pres);
+            Marshal.ReleaseComObject(app);
+        }
+
+        private void btnGenerarword_Click(object sender, EventArgs e)
+        {
+            string titulo = txtTema.Text;
+            string contenido = txtResultado.Text;
+            if (string.IsNullOrWhiteSpace(contenido))
+            {
+                MessageBox.Show("No hay contenido para generar Word.");
+                return;
+            }
+
+            CrearWord(titulo, contenido, "Generado automáticamente por la IA.");
+            MessageBox.Show("Documento Word generado correctamente.");
+        }
+
+        private void btnGenerarPPT_Click(object sender, EventArgs e)
+        {
+            string titulo = txtTema.Text;
+            string contenido = txtResultado.Text;
+            if (string.IsNullOrWhiteSpace(contenido))
+            {
+                MessageBox.Show("No hay contenido para generar PowerPoint.");
+                return;
+            }
+
+            CrearPowerPoint(titulo, contenido);
+            MessageBox.Show("Presentación PowerPoint generada correctamente.");
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtTema.Clear();//para limpiar el campo de texto
+            txtResultado.Clear();//para limpiar el campo de texto de la respuesta
+            txtTema.Focus();//para que el cursor se posicione en el campo de texto
+        }       
+
+    }
+}
+
+
+
